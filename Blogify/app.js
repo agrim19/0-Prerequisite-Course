8de287e27e96f5d178d4a5a8bb6cc82b47d251a5
Agrim <agrim19229@iiitd.ac.in>
2020-07-12 11:05:02 +0530

    improving new page

--- Blogify/app.js
+++ Blogify/app.js
@@ -3,60 +3,57 @@ const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-app.use(bodyParser.urlencoded({extended:true}));
-app.set('view engine','ejs');
-app.use(express.static(__dirname+'/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
 
 //connect to db
-mongoose.connect('mongodb://localhost:27017/Blogify',{useNewUrlParser:true,useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost:27017/Blogify', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 //make schema
 const blogSchema = mongoose.Schema({
-	title: String,
-	content: String,
+    title: String,
+    content: String,
+    image: String,
 });
 //model
-const Blog = mongoose.model('Blog',blogSchema);    //blogs
+const Blog = mongoose.model('Blog', blogSchema); //blogs
 
-
-//routes 
-app.get('/',(req,res)=>{
-	Blog.find({}, (err,data)=>{
-		if(err)
-			console.log(err);
-		res.render('home',{data:data});	
-	})
+//routes
+app.get('/', (req, res) => {
+    Blog.find({}, (err, data) => {
+        if (err) console.log(err);
+        res.render('home', { data: data });
+    });
 });
-app.get('/blogs',(req,res)=>{
-	Blog.find({}, (err,data)=>{
-		if(err)
-			console.log(err);
-		res.render('index',{data:data});	
-	})
+app.get('/blogs', (req, res) => {
+    Blog.find({}, (err, data) => {
+        if (err) console.log(err);
+        res.render('index', { data: data });
+    });
 });
-app.get('/blogs/new',(req,res)=>{
-	res.render('new');
+app.get('/blog/new', (req, res) => {
+    res.render('new');
 });
-app.get('/blogs/:id', (req,res)=>{
-	Blog.findById(req.params.id,(err,foundBlog)=>{
-		if(err)
-			console.log(err);
-		res.render('show',{blog:foundBlog});
-	})
-})
+app.get('/blogs/:id', (req, res) => {
+    Blog.findById(req.params.id, (err, foundBlog) => {
+        if (err) console.log(err);
+        res.render('show', { blog: foundBlog });
+    });
+});
 
-app.post('/blogs',(req,res)=>{
-	const newPost = req.body;
-	Blog.create(newPost,(err,createdBlog)=>{
-		if(err)
-			console.log(err);	
-		console.log(createdBlog);
-		res.redirect('/blogs');	
-	})
-})
-
-
+app.post('/blogs', (req, res) => {
+    const newPost = req.body;
+    Blog.create(newPost, (err, createdBlog) => {
+        if (err) console.log(err);
+        console.log(createdBlog);
+        res.redirect('/blogs');
+    });
+});
 
 //server
-app.listen(3000,()=>{
-	console.log('Serving Blogify on Port 3000');
+app.listen(3000, () => {
+    console.log('Serving Blogify on Port 3000');
 });
