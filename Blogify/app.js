e21ad26fb079536cbdf6fa5d9db99f896253818a
Agrim <agrim19229@iiitd.ac.in>
2020-07-16 18:28:05 +0530

    add login signup logout functionality

--- Blogify/app.js
+++ Blogify/app.js
@@ -1,12 +1,32 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const passport = require('passport');
+const User = require('./models/user');
+const bodyParser = require('body-parser');
 const blogRoutes = require('./routes/blogs');
 const indexRoutes = require('./routes/index');
+const LocalStrategy = require('passport-local');
+const ExpressSession = require('express-session');
 const commentRoutes = require('./routes/comments');
+const passportLocalMongoose = require('passport-local-mongoose');
 
-app.use(bodyParser.urlencoded({extended: true}));
+//passport setup
+app.use(
+    ExpressSession({
+        secret: 'This is the blogify app',
+        resave: false,
+        saveUninitialized: false,
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//general app.use and app.set statements
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
--- Blogify/routes/index.js
+++ Blogify/routes/index.js
@@ -1,12 +1,50 @@
 const express = require('express');
 const router = express.Router();
 const Blog = require('../models/blog');
+const User = require('../models/user');
+const passport = require('passport');
 
 router.get('/', (req, res) => {
     Blog.find({}, (err, data) => {
         if (err) console.log(err);
-        res.render('home', {data: data});
+        res.render('home', { data: data });
     });
 });
 
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+
+router.get('/register', (req, res) => {
+    res.render('register');
+});
+
+router.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/blogs');
+});
+
+router.post('/register', (req, res) => {
+    User.register(
+        new User({ username: req.body.username, email: req.body.email }),
+        req.body.password,
+        (err, user) => {
+            if (err) {
+                console.log(err);
+                return res.render('register');
+            }
+            passport.authenticate('local')(req, res, () => {
+                res.redirect('/blogs');
+            });
+        }
+    );
+});
+router.post(
+    '/login',
+    passport.authenticate('local', {
+        failureRedirect: '/login',
+        successRedirect: '/blogs',
+    }),
+    (req, res) => {}
+);
 module.exports = router;
